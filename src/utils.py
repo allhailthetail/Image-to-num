bc49bc952298e8dba1773f8923b02c8d30ee482c
Matthew Younger <matthewyounger33@gmail.com>
2025-10-19 08:33:09 -0500

    Prepping the add of .YAML config

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@
 import sys
 from PIL import Image, ImageOps, ImageEnhance, ImageFilter # used by preprocess_image()
 
-def preprocess_image(input_path, output_path=None):
+def preprocess_image(input_path, output_path=None, crop_tuple=None):
     '''
     Description:
     Function processes the image before it's sent to Tesseract for text extraction.
@@ -17,6 +17,7 @@ def preprocess_image(input_path, output_path=None):
     Parameters:
     input_path (str): File being processed
     output_path=None (str): Output can be changed if desired...
+    crop_tuple=None  (tup: Int): Cropping coordinates
 
     Output:
     processed (PIL.Image.Image): Enhanced/adjusted image object
@@ -25,39 +26,48 @@ def preprocess_image(input_path, output_path=None):
     # First open the image
     img = Image.open(input_path).convert("RGB")
 
-    # Smaller resolution (This is to run it on my CPU faster, but on a GPU this part of the code
-    # could be removed and the application should work better with a better image resolution)
-    base_width = 800 # 800 optimal
-    w_percent = (base_width / float(img.size[0]))
-    h_size = int((float(img.size[1])* float(w_percent)))
-    img = img.resize((base_width, h_size), Image.Resampling.LANCZOS)
+    # Scale the image down to fewer pixels for processing efficiency:
+    BASE_WIDTH = 800 # (const) Target width (default recommended: 800px)
+    orig_width, orig_height = img.size
+
+    # Maintain aspect ratio by scaling proportionately
+    scale_ratio = BASE_WIDTH / float(orig_width)
+    new_height = int(orig_height * scale_ratio)
+
+    # Finally, resize the image with pillow:
+    img = img.resize((BASE_WIDTH, new_height), Image.Resampling.LANCZOS)
 
     # Crop it
-    width, height = img.size
-    left = int(0.1 * width) #0.033 For testing images, 0.05 Broken ones,0.1 Latest
-    top = int(0.4 * height)
-    right = int(0.80 * width) # 0.75 Before # 0.80 optimal
-    bottom = int(0.75 * height) # Just height before
-    cropped = img.crop((left, top, right, bottom))
+    # NOTE: (From the Pillow docs)
+    #   The Python Imaging Library uses a Cartesian pixel coordinate system, with (0,0) in the upper left corner.
+    #   Note that the coordinates refer to the implied pixel corners; the centre of a pixel addressed as (0, 0)
+    #   actually lies at (0.5, 0.5).
+    #   Coordinates are usually passed to the library as 2-tuples (x, y). Rectangles are represented as 4-tuples,
+    #   (x1, y1, x2, y2), with the upper left corner given first.
+    #   A good reference: (80, 240, 640, 450) at time of development...
 
-    # Convert grayscale
-    gray = cropped.convert("L")
-    #gray = img.convert("L")
+    # Only crop if cropping coordinates are provided
+    if crop_tuple:
+        # Crops to the tuple provided to the function
+        img = img.crop(crop_tuple)
+
+    # Convert to gray scale
+    img = img.convert("L")
 
     # Invert the colors
-    inverted = ImageOps.invert(gray)
+    img = ImageOps.invert(img)
 
     # apply Gaussian blur
-    blurred = inverted.filter(ImageFilter.GaussianBlur(1.5)) # 3.5 optimal, 1.5 seems to work fine
+    img = img.filter(ImageFilter.GaussianBlur(1.5)) # 3.5 optimal, 1.5 seems to work fine
 
     # Increase contrast
-    enhancer = ImageEnhance.Contrast(blurred)
-    processed = enhancer.enhance(15) # 10 optimal
+    enhancer = ImageEnhance.Contrast(img)
+    img = enhancer.enhance(15) # 10 optimal
 
     if output_path:
-        processed.save(output_path)
+        img.save(output_path)
 
-    return processed
+    return img
 
 
 if __name__ == "__main__":
@@ -78,5 +88,5 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         output_img = sys.argv[2]
 
-    processed_img = preprocess_image(input_img, output_img)
+    processed_img = preprocess_image(input_img, output_path=output_img)
     print("Processing Complete.")
